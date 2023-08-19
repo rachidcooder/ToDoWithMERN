08854f2b165567d7f6713572170eb3ac3ddfe9ba
rachidcooder <cooderherou@gmail.com>
2023-08-19 18:41:31 +0100

    commit befor deploying

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,33 @@ import path from "path"
  
  dotenv.config();
  const app=express();
+  
  const PORT=process.env.PORT||5000
+
+   
  app.use(cors());
  app.use(express.json());
 connectDB();
  
  app.use("/api/todo",route)
 
-// -----------------deployment--------------------------
+ // --------------------------deployment------------------------------
 
-// -----------------deployment--------------------------
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "../","frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
  app.listen(PORT,()=>{
   console.log("app listening to  port ",PORT);
